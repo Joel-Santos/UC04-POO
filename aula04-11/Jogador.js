d938e2885f61403d0c4a65961a7e82173527acea
Joel Santos <i3975@rn.senac.local>
2024-11-05 16:58:08 -0300

    Aula 05-11

--- aula04-11/Jogador.js
+++ aula04-11/Jogador.js
@@ -1,4 +1,28 @@
 class Jogador{
-
+    #nome;
+    #aposta;
+    constructor(nome, aposta){
+        this.#nome = nome;
+        this.#aposta = aposta;
+        Object.freeze(this);
+    }
+    get getNome(){
+        return this.#nome;
+    }
+    set setNome(nome){
+        this.#nome = nome;
+    }
+    get getAposta(){
+        return this.#aposta;
+    }
+    set setAposta(aposta){
+        this.#aposta = aposta;
+    }
+    aprentarDados(){
+        console.log(`Nome do jogador: ${this.getNome}, Aposta: ${this.getAposta}`);
+    }
 }
 module.exports = {Jogador}
+// let jogador01 = new Jogador('Jorel', 4);
+// // console.log(jogador01.getNome);
+// jogador01.aprentarDados();
--- aula04-11/SistemaAzar.js
+++ aula04-11/SistemaAzar.js
@@ -1,4 +1,38 @@
+const {Dado} = require('./Dado');
+const {Jogador} = require('./Jogador');
 class SistemaAzar{
+    #dado;
+    #jogador;
+    constructor(dado, jogador){
+        if(dado instanceof Dado && jogador instanceof Jogador){//verificando se os paramêtros são do mesmo tipo (Jogador e Dado)
+            this.#dado = dado;
+            this.#jogador = jogador;
+        }else{
+            console.log('Os dados fornecidos não são do Tipo Jogador ou Dado');
+        }
+        Object.freeze(this); //Bloqueando a criação de novos atributos 
+    }
+    get getDado(){
+        return this.#dado;
+    }
+    set setDado(dado){
+        this.#dado = dado;
+    }
+    get getJogador(){
+        return this.#jogador;
+    }
+    set setJogador(jogador){
+        this.#jogador = jogador;
+    }
+    verificarGanhador(){
+            if(this.#dado.getFace === this.#jogador.getAposta){
+            console.log(`Parabéns Você ganhou! Resultado do SistemaAzar: ${this.#dado.getFace}`);
+            this.#jogador.aprentarDados();
+        }else{
+            console.log(`Que Pena! Aposte novamente. Resultado do SistemaAzar: ${this.#dado.getFace}`);
+            this.#jogador.aprentarDados();    
+        }
+    }
 
 }
 module.exports = {SistemaAzar}
